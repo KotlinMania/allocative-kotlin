b9c0f6e252b4971a692bcd99275df7a286bc0a89
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-24 09:52:56 -0700

    test(allocative): add AllocativeTraitTest suite for visitor interface

---
+++ src/commonTest/kotlin/io/github/kotlinmania/allocative/AllocativeTraitTest.kt
@@ -0,0 +1,23 @@
+// port-lint: tests allocative_trait.rs
+package io.github.kotlinmania.allocative
+
+import io.github.kotlinmania.allocative.key.Key
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AllocativeTraitTest {
+    private class SimpleNode(val size: Int) : Allocative {
+        override fun visit(visitor: Visitor) {
+            val self = visitor.enterSelf(this, size)
+            self.visitSimple(Key.new("size"), 4)
+            self.exit()
+        }
+    }
+
+    @Test
+    fun testAllocativeInterface() {
+        val node = SimpleNode(16)
+        assertEquals(0, sizeOfUniqueAllocatedData(node))
+        assertEquals(16, sizeOfUnique(node, 16))
+    }
+}
